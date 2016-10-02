86b2d6fcb6995ce1639f974100c7ebeff4bf6df8
Yaroslav Shumlianskyi <i.am.LLluma@gmail.com>
2016-10-02 23:05:33 +0300

    Add first unit test

---
+++ test/test_misc.py
@@ -0,0 +1,22 @@
+import os
+import unittest
+
+from pycnn import pycnn
+
+BASE_DIR = os.path.abspath(os.path.join(
+    os.path.join(os.path.dirname(__file__)),
+    '..',
+))
+
+
+class TestMiscFunctions(unittest.TestCase):
+
+    def setUp(self):
+        self.cnn = pycnn()
+
+    def test_is_valid(self):
+        input_file = os.path.join(
+            BASE_DIR, 'images', 'input.bmp',
+        )
+        result = self.cnn.isvalid(input_file)
+        self.assertTrue(result)
