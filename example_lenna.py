1f5af74dc2a1be483864bca314e5fd4196993e73
Ankit Aggarwal <ankitaggarwal011@gmail.com>
2016-10-06 00:29:59 +0530

    Added back examples and removed tox.
    Tox have been upgraded with complete unit test and Travis CI.

---
+++ example_lenna.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from pycnn import pycnn
+
+# Initialize object
+cnn = pycnn()
+
+# Perform respective image processing techniques on the given image
+
+cnn.edgedetection('images/lenna.gif', 'images/lenna_edge.png')
+cnn.diagonallinedetection('images/lenna.gif', 'images/lenna_diagonal.png')
