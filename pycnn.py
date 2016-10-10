29d1ab5b0b6661e698065caf0ec153770a66d447
Iraquitan Cordeiro Filho <iraquitanfilho@gmail.com>
2016-10-10 11:34:11 -0300

    Added Google Style Python Docstrings to the module (#30)
    Closes #27.
    
    Add Google Style Python docstrings to the module, classes, and functions.

--- pycnn.py
+++ pycnn.py
@@ -41,20 +41,62 @@ warnings.filterwarnings('ignore')  # Ignore trivial warnings
 
 
 class pycnn(object):
+    """Image Processing with Cellular Neural Networks (CNN).
+
+    Cellular Neural Networks (CNN) are a parallel computing paradigm that was
+    first proposed in 1988. Cellular neural networks are similar to neural
+    networks, with the difference that communication is allowed only between
+    neighboring units. Image Processing is one of its applications. CNN
+    processors were designed to perform image processing; specifically, the
+    original application of CNN processors was to perform real-time ultra-high
+    frame-rate (>10,000 frame/s) processing unachievable by digital processors.
+
+    This python library is the implementation of CNN for the application of
+    Image Processing.
+
+
+    Attributes:
+        n (int): Height of the image.
+        m (int): Width of the image.
+    """
 
     def __init__(self):
+        """Sets the initial class attributes m (width) and n (height)."""
         self.m = 0  # width (number of columns)
         self.n = 0  # height (number of rows)
 
     def f(self, x, t, Ib, Bu, tempA):
+        """Computes the derivative of x at t.
+
+        Args:
+            x: The input.
+            Ib (float): System bias.
+            Bu: Convolution of control template with input.
+            tempA (:obj:`list` of :obj:`list`of :obj:`float`): Feedback
+                template.
+        """
         x = x.reshape((self.n, self.m))
         dx = -x + Ib + Bu + sig.convolve2d(self.cnn(x), tempA, 'same')
         return dx.reshape(self.m * self.n)
 
     def cnn(self, x):
+        """Piece-wise linear sigmoid function.
+
+        Args:
+            x : Input to the piece-wise linear sigmoid function.
+        """
         return 0.5 * (abs(x + 1) - abs(x - 1))
 
     def validate(self, input_location):
+        """Checks if a string path exists or is from a supported file type.
+
+        Args:
+            input_location (str): A string with the path to the image.
+
+        Raises:
+            IOError: If `input_location` does not exist or is not a file.
+            Exception: If file type is not supported.
+        """
         _, ext = os.path.splitext(input_location)
         ext = ext.lstrip('.').lower()
         if not os.path.exists(input_location):
@@ -71,6 +113,21 @@ class pycnn(object):
     # tempA: feedback template, tempB: control template
     def imageprocessing(self, inputlocation, outputlocation,
                         tempA, tempB, initialcondition, Ib, t):
+        """Process the image with the input arguments.
+
+        Args:
+            inputlocation (str): The string path for the input image.
+            outputlocation (str): The string path for the output processed
+                image.
+            tempA (:obj:`list` of :obj:`list`of :obj:`float`): Feedback
+                template.
+            tempB (:obj:`list` of :obj:`list`of :obj:`float`): Control
+                template.
+            initialcondition (float): The initial state.
+            Ib (float): System bias.
+            t (numpy.ndarray): A numpy array with evenly spaced numbers
+                representing time points.
+        """
         gray = img.open(inputlocation).convert('RGB')
         self.m, self.n = gray.size
         u = np.array(gray)
@@ -106,6 +163,22 @@ class pycnn(object):
                          initialcondition=0.0,
                          Ib_b=0.0,
                          t=np.linspace(0, 10.0, num=2)):
+        """Validate and process the image with the input arguments.
+
+        Args:
+            name (str): The name of the template.
+            inputlocation (str): The string path for the input image.
+            outputlocation (str): The string path for the output processed
+                image.
+            tempA_A (:obj:`list` of :obj:`list`of :obj:`float`): Feedback
+                template.
+            tempB_B (:obj:`list` of :obj:`list`of :obj:`float`): Control
+                template.
+            initialcondition (float): The initial state.
+            Ib_b (float): System bias.
+            t (numpy.ndarray): A numpy array with evenly spaced numbers
+                representing time points.
+        """
         self.validate(inputlocation)
         print(name, 'initialized.')
         self.imageprocessing(inputlocation,
@@ -119,6 +192,28 @@ class pycnn(object):
         print('Result is saved at %s.\n' % (outputlocation))
 
     def edgedetection(self, inputlocation='', outputlocation='output.png'):
+        """Performs Edge Detection on the input image.
+
+        The output is a binary image showing all edges of the input image in
+        black.
+
+        A = [[0.0 0.0 0.0],
+             [0.0 1.0 0.0],
+             [0.0 0.0 0.0]]
+
+        B = [[−1.0 −1.0 −1.0],
+             [−1.0 8.0 −1.0],
+             [−1.0 −1.0 −1.0]]
+
+        z = −1.0
+
+        Initial state = 0.0
+
+        Args:
+            inputlocation (str): The string path for the input image.
+            outputlocation (str): The string path for the output processed
+                image.
+        """
         name = 'Edge detection'
         tempA = [[0.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 0.0]]
         tempB = [[-1.0, -1.0, -1.0], [-1.0, 8.0, -1.0], [-1.0, -1.0, -1.0]]
@@ -140,6 +235,28 @@ class pycnn(object):
 
     def grayscaleedgedetection(self, inputlocation='',
                                outputlocation='output.png'):
+        """Performs Gray-scale Edge Detection on the input image.
+
+        The output is a Gray-scale image showing an edge map of the input
+        image in black.
+
+        A = [[0.0 0.0 0.0],
+             [0.0 2.0 0.0],
+             [0.0 0.0 0.0]]
+
+        B = [[−1.0 −1.0 −1.0],
+             [−1.0 8.0 −1.0],
+             [−1.0 −1.0 −1.0]]
+
+        z = −0.5
+
+        Initial state = 0.0
+
+        Args:
+            inputlocation (str): The string path for the input image.
+            outputlocation (str): The string path for the output processed
+                image.
+        """
         name = 'Grayscale edge detection'
         tempA = [[0.0, 0.0, 0.0], [0.0, 2.0, 0.0], [0.0, 0.0, 0.0]]
         tempB = [[-1.0, -1.0, -1.0], [-1.0, 8.0, -1.0], [-1.0, -1.0, -1.0]]
@@ -157,6 +274,28 @@ class pycnn(object):
             t)
 
     def cornerdetection(self, inputlocation='', outputlocation='output.png'):
+        """Performs Corner Detection on the input image.
+
+        The output is a binary image where black pixels represent the convex
+        corners of objects in the input image.
+
+        A = [[0.0 0.0 0.0],
+             [0.0 1.0 0.0],
+             [0.0 0.0 0.0]]
+
+        B = [[−1.0 −1.0 −1.0],
+             [−1.0 4.0 −1.0],
+             [−1.0 −1.0 −1.0]]
+
+        z = −5.0
+
+        Initial state = 0.0
+
+        Args:
+            inputlocation (str): The string path for the input image.
+            outputlocation (str): The string path for the output processed
+                image.
+        """
         name = 'Corner detection'
         tempA = [[0.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 0.0]]
         tempB = [[-1.0, -1.0, -1.0], [-1.0, 4.0, -1.0], [-1.0, -1.0, -1.0]]
@@ -175,6 +314,28 @@ class pycnn(object):
 
     def diagonallinedetection(self, inputlocation='',
                               outputlocation='output.png'):
+        """Performs Diagonal Line-Detection on the input image.
+
+        The output is a binary image representing the locations of diagonal
+        lines in the input image.
+
+        A = [[0.0 0.0 0.0],
+             [0.0 1.0 0.0],
+             [0.0 0.0 0.0]]
+
+        B = [[−1.0 0.0 −1.0],
+             [0.0 1.0 0.0],
+             [1.0 0.0 −1.0]]
+
+        z = −4.0
+
+        Initial state = 0.0
+
+        Args:
+            inputlocation (str): The string path for the input image.
+            outputlocation (str): The string path for the output processed
+                image.
+        """
         name = 'Diagonal line detection'
         tempA = [[0.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 0.0]]
         tempB = [[-1.0, 0.0, 1.0], [0.0, 1.0, 0.0], [1.0, 0.0, -1.0]]
@@ -192,6 +353,25 @@ class pycnn(object):
             t)
 
     def inversion(self, inputlocation='', outputlocation='output.png'):
+        """Performs Inversion (Logic NOT) on the input image.
+
+        A = [[0.0 0.0 0.0],
+             [0.0 1.0 0.0],
+             [0.0 0.0 0.0]]
+
+        B = [[0.0 0.0 0.0],
+             [1.0 1.0 1.0],
+             [0.0 0.0 0.0]]
+
+        z = −2.0
+
+        Initial state = 0.0
+
+        Args:
+            inputlocation (str): The string path for the input image.
+            outputlocation (str): The string path for the output processed
+                image.
+        """
         name = 'Inversion'
         tempA = [[0.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 0.0]]
         tempB = [[0.0, 0.0, 0.0], [1.0, 1.0, 1.0], [0.0, 0.0, 0.0]]
